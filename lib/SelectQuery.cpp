452b9d6eef124939a1729b8e76c89248f4ab0452
mdatsev <martin.datsev.p@gmail.com>
2020-03-10 19:51:19 +0200

    condition classes

--- lib/SelectQuery.cpp
+++ lib/SelectQuery.cpp
@@ -2,11 +2,21 @@
 #include <vector>
 
 #include "SelectQuery.h"
-
+#include "errors.h"
 #include "ParserHelper.h"
 
 SelectQuery SelectQuery::parse(std::string input) {
   return SelectQuery(input);
+}
+
+Op parseOp(std::string str) {
+  if (str == "<=") return Op::le;
+  if (str == ">=") return Op::ge;
+  if (str == "<" ) return Op::lt;
+  if (str == ">" ) return Op::gt;
+  if (str == "==") return Op::eq;
+  if (str == "!=") return Op::ne;
+  throw UserError("Invalid operator");
 }
 
 SelectQuery::SelectQuery(std::string input) {
@@ -22,4 +32,14 @@ SelectQuery::SelectQuery(std::string input) {
   }
   ph.get_word();  // FROM
   table_name = ph.get_word();
+  auto next_clause = ph.get_word(); // WHERE
+  if (next_clause.size() != 0) {
+    conditional = true;
+    // for now always [column] [operator] [value]
+    condition.column = ph.get_word();
+    condition.op = parseOp(ph.get_token());
+    condition.value = ph.get_literal();
+  } else {
+    conditional = false;
+  }
 }
--- lib/SelectQuery.h
+++ lib/SelectQuery.h
@@ -1,10 +1,27 @@
 #pragma once
 
+enum class Op {
+  le,
+  ge,
+  lt,
+  gt,
+  eq,
+  ne
+};
+
+struct Condition {
+  std::string column;
+  Op op;
+  std::string value;
+};
+
 class SelectQuery {
  public:
   std::string table_name;
   std::vector<std::string> fields;
   static SelectQuery parse(std::string input);
+  bool conditional;
+  Condition condition;
 
  private:
   SelectQuery(std::string input);
